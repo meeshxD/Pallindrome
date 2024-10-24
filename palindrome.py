88470607ea152a94d0713b35487636c51c955ba9
meeshxD <lo.michelle0810@gmail.com>
2024-10-24 00:42:45 -0400

    function

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,11 @@
 """
 Validates strings as palindromes.
 """
-def is_palindrome(string):
-    
+def is_palindrome(input_string):
+    if (not isinstance(input_string, str)):
+        raise ValueError("Wrong data type, must be a string")
+    lower_string = str.lower(input_string)
+    lower_space_string = lower_string.replace(" ","")
+    reverse_input = lower_space_string[::-1]
+    if lower_space_string == reverse_input:
+        return lower_space_string == reverse_input
