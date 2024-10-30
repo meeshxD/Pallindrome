ef54df64895d85a482ca7b6be265a7e5695bafef
meeshxD <lo.michelle0810@gmail.com>
2024-10-30 19:33:13 -0400

    deque

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,16 @@
 """
 Validates strings as palindromes.
 """
+from collections import deque
 def is_palindrome(input_string):
     if (not isinstance(input_string, str)):
         raise ValueError("Wrong data type, must be a string")
     lower_string = str.lower(input_string)
     lower_space_string = lower_string.replace(" ","")
-    reverse_input = lower_space_string[::-1]
-    if lower_space_string == reverse_input:
-        return lower_space_string == reverse_input
+    deq_input = deque(lower_space_string)
+    reverse_deq_input = deque(reversed(deq_input))
+    if deq_input == reverse_deq_input:
+        return deq_input == reverse_deq_input
     else:
         return False
+
