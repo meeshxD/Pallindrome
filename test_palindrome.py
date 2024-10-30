1aa192c060aa67aeda880d8d13673a887ee9d732
meeshxD <lo.michelle0810@gmail.com>
2024-10-30 19:41:05 -0400

    test 1
    value that is not a string

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,8 @@
 """
 Tests the palindrome module
 """
+import pytest
+from palindrome import is_palindrome
+def test_string():
+    with pytest.raises(ValueError, match="Wrong data type, must be a string"):
+        is_palindrome(12321)
